a7ecdaa810021f6f725f476db700696110b35adc
dlsxody1 <dlsxody1@naver.com>
2025-04-08 21:10:28 +0900

    chore : vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import { resolve } from "path";
@@ -6,7 +7,8 @@ import react from "@vitejs/plugin-react-swc";
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
-    base: "/",
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "/",
     build: {
       rollupOptions: {
         input: {
@@ -26,5 +28,5 @@ export default mergeConfig(
         reporter: ["lcov", "json", "json-summary"],
       },
     },
-  }),
+  })
 );
